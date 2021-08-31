d5dffe8111fdc3287f0f89621e42c325e25c5877
Junaid Afzal <junaidafzal7274@gmail.com>
2021-08-31 19:45:10 +0500

    first

--- My4thApp/SerializerTry/urls.py
+++ My4thApp/SerializerTry/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('',views.mainS),
+    path('student-list',views.return_student)
 ]
--- My4thApp/SerializerTry/views.py
+++ My4thApp/SerializerTry/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,JsonResponse
 from django.http import request
+from .models import Student
+from .serializer import StudentSerializer
 # Create your views here.
 
 
@@ -8,4 +10,11 @@ from django.http import request
 def mainS(request):
     #return HttpResponse('<H1>Hello Main SerializerTry</H1>')
     return render(request,'index.html')
+
+def return_student(request):
+    std = Student.objects.all()
+    std_ser = StudentSerializer(std)
+
+    return JsonResponse(std_ser.data,many=True)
+
 
